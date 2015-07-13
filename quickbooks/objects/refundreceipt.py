5743796d00a4a49efe86c05e65ab2f01b342f8d8
ej2 <edward.emanuel@gmail.com>
2015-07-13 14:46:18 -0500

    Added Refund Receipt object.

---
+++ quickbooks/objects/refundreceipt.py
@@ -0,0 +1,57 @@
+from base import QuickbooksBaseObject, Ref, CustomField
+from tax import TxnTaxDetail
+
+
+
+class SalesItemLineDetail(QuickbooksBaseObject):
+    class_dict = {
+        "ItemRef": Ref,
+        "TaxCodeRef": Ref
+    }
+
+    def __init__(self):
+        self.ItemRef = None
+        self.TaxCodeRef = None
+
+
+class RefundReceiptLine(QuickbooksBaseObject):
+    class_dict = {
+        "SalesItemLineDetail": SalesItemLineDetail
+    }
+
+    def __init__(self):
+        self.LineNum = ""
+        self.Amount = 0
+        self.DetailType = ""
+
+        self.SalesItemLineDetail = None
+
+
+class RefundReceipt(QuickbooksBaseObject):
+    """
+    QBO definition: RefundReceipt represents a refund to the customer for a product or service that was given.
+    """
+    class_dict = {
+        "DepartmentRef": Ref,
+        "TxnTaxDetail": TxnTaxDetail,
+        "DepositToAccountRef": Ref
+    }
+
+    list_dict = {
+        "CustomField": CustomField,
+        "Line": RefundReceiptLine,
+    }
+
+    def __init__(self):
+        self.DocNumber = ""
+        self.TotalAmt = 0
+        self.ApplyTaxAfterDiscount = False
+        self.PrintStatus = ""
+        self.Balance = 0
+        self.PaymentRefNum = ""
+
+        self.DepartmentRef = None
+        self.TxnTaxDetail = None
+        self.DepositToAccountRef = None
+        self.CustomField = []
+        self.Line = []
