5a77f417ca802b27c67f6021c6b59b7ea6e20974
ej2 <edward.emanuel@gmail.com>
2015-08-16 09:48:39 -0500

    Added tests and unicode to PaymentMethod.

--- tests/unit/objects/test_payment.py
+++ tests/unit/objects/test_payment.py
@@ -5,17 +5,17 @@ from quickbooks.objects.payment import PaymentLine, Payment
 
 class PaymentLineTests(unittest.TestCase):
     def test_unicode(self):
-        memo_line = PaymentLine()
-        memo_line.LineNum = 1
-        memo_line.Description = "Product Description"
-        memo_line.Amount = 100
+        payment_line = PaymentLine()
+        payment_line.LineNum = 1
+        payment_line.Description = "Product Description"
+        payment_line.Amount = 100
 
-        self.assertEquals(memo_line.__unicode__(), "[1] Product Description 100")
+        self.assertEquals(payment_line.__unicode__(), "[1] Product Description 100")
 
 
 class PaymentTests(unittest.TestCase):
     def test_unicode(self):
-        credit_memo = Payment()
-        credit_memo.TotalAmt = 1000
+        payment = Payment()
+        payment.TotalAmt = 1000
 
-        self.assertEquals(credit_memo.__unicode__(), 1000)
+        self.assertEquals(payment.__unicode__(), 1000)
---
+++ tests/unit/objects/test_paymentmethod.py
@@ -0,0 +1,11 @@
+import unittest
+
+from quickbooks.objects.paymentmethod import PaymentMethod
+
+
+class PaymentMethodTests(unittest.TestCase):
+    def test_unicode(self):
+        payment_method = PaymentMethod()
+        payment_method.Name = "test"
+
+        self.assertEquals(payment_method.__unicode__(), "test")
