b022f4c73aef3632dab0a54bacb023544967c74a
ej2 <edward.emanuel@gmail.com>
2015-09-10 11:16:29 -0500

    Updated Department.

--- quickbooks/objects/department.py
+++ quickbooks/objects/department.py
@@ -1,16 +1,20 @@
-from base import QuickbooksManagedObject, QuickbooksTransactionEntity
+from base import QuickbooksManagedObject, QuickbooksTransactionEntity, Ref
 
 
 class Department(QuickbooksManagedObject, QuickbooksTransactionEntity):
     """
-    QBO definition: The Department entity provides a way to track different segments of the business, divisions, or physical locations
-    such as stores, and allows another way of categorizing the entire transaction. This is in contrast to Class objects,
-    which are applied to individual transaction line details. Delete is achieved by setting the Active attribute to
-    false in an entity update request; thus, making it inactive. In this type of delete, the record is not permanently
-    deleted, but is hidden for display purposes. References to inactive objects are left intact.
+    QBO definition: The Department entity provides a way to track different segments of the business, divisions, or
+    physical locations such as stores, and allows another way of categorizing the entire transaction.
+    This is in contrast to Class objects, which are applied to individual transaction line details.
+    Delete is achieved by setting the Active attribute to false in an entity update request; thus,
+    making it inactive. In this type of delete, the record is not permanently deleted, but is hidden
+    for display purposes. References to inactive objects are left intact.
     """
 
-    class_dict = {}
+    class_dict = {
+        "ParentRef": Ref
+    }
+
     qbo_object_name = "Department"
 
     def __init__(self):
@@ -21,4 +25,13 @@ class Department(QuickbooksManagedObject, QuickbooksTransactionEntity):
         self.Active = True
 
     def __unicode__(self):
-        return self.FullyQualifiedName
+        return self.Name
+
+    def to_ref(self):
+        ref = Ref()
+
+        ref.name = self.Name
+        ref.type = self.qbo_object_name
+        ref.value = self.Id
+
+        return ref
--- tests/unit/objects/test_department.py
+++ tests/unit/objects/test_department.py
@@ -6,6 +6,17 @@ from quickbooks.objects.department import Department
 class DepartmentTests(unittest.TestCase):
     def test_unicode(self):
         department = Department()
-        department.FullyQualifiedName = "test"
+        department.Name = "test"
 
         self.assertEquals(unicode(department), "test")
+
+    def test_to_ref(self):
+        department = Department()
+        department.Name = "test"
+        department.Id = 100
+
+        dept_ref = department.to_ref()
+
+        self.assertEquals(dept_ref.name, "test")
+        self.assertEquals(dept_ref.type, "Department")
+        self.assertEquals(dept_ref.value, 100)
