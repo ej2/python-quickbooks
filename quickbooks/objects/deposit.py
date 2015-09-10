4a3b895ba61c1f7464fc29a8e6db12b93b0eb268
ej2 <edward.emanuel@gmail.com>
2015-09-10 11:38:33 -0500

    Updated Deposit.

--- quickbooks/objects/deposit.py
+++ quickbooks/objects/deposit.py
@@ -1,27 +1,81 @@
-from base import QuickbooksBaseObject, Ref, LinkedTxn, QuickbooksManagedObject, LinkedTxnMixin
+from base import QuickbooksBaseObject, Ref, LinkedTxn, QuickbooksManagedObject, LinkedTxnMixin, \
+    QuickbooksTransactionEntity, CustomField
+
+
+class AttachableRef(QuickbooksBaseObject):
+    class_dict = {
+        "EntityRef": Ref,
+    }
+
+    list_dict = {
+        "CustomField": CustomField
+    }
+
+    def __init__(self):
+        super(AttachableRef, self).__init__()
+        self.LineInfo = ""
+        self.IncludeOnSend = False
+        self.Inactive = False
+        self.NoRefOnly = False
+        self.EntityRef = None
+
+        self.CustomField = []
+
+
+class CashBackInfo(QuickbooksBaseObject):
+    class_dict = {
+        "AccountRef": None
+    }
+
+    def __init__(self):
+        super(CashBackInfo, self).__init__()
+        self.Amount = 0
+        self.Memo = ""
+        self.AccountRef = None
+
+
+class DepositLineDetail(QuickbooksBaseObject):
+    class_dict = {
+        "Entity": Ref,
+        "ClassRef": Ref,
+        "AccountRef": Ref,
+        "PaymentMethodRef": Ref,
+    }
+
+    def __init__(self):
+        super(DepositLineDetail, self).__init__()
+        self.CheckNum = ""
+        self.TxnType = ""
 
 
 class DepositLine(QuickbooksBaseObject):
     class_dict = {
-        "DepositToAccountRef": Ref
+        "DepositToAccountRef": Ref,
+        "DepositLineDetail": DepositLineDetail,
     }
 
     list_dict = {
-        "LinkedTxn": LinkedTxn
+        "LinkedTxn": LinkedTxn,
+        "CustomField": CustomField,
     }
 
     qbo_object_name = "Deposit"
 
     def __init__(self):
         super(DepositLine, self).__init__()
+        self.Id = 0
+        self.LineNum = 0
+        self.Description = ""
         self.Amount = 0
+        self.DetailType = "DepositLineDetail"
         self.LinkedTxn = []
+        self.CustomField = []
 
     def __unicode__(self):
         return str(self.Amount)
 
 
-class Deposit(QuickbooksManagedObject, LinkedTxnMixin):
+class Deposit(QuickbooksManagedObject, QuickbooksTransactionEntity, LinkedTxnMixin):
     """
     QBO definition: A deposit object is a transaction that records one or more deposits of the following types:
 
@@ -33,7 +87,11 @@ class Deposit(QuickbooksManagedObject, LinkedTxnMixin):
     """
 
     class_dict = {
-        "DepositToAccountRef": Ref
+        "DepositToAccountRef": Ref,
+        "DepartmentRef": Ref,
+        "CurrencyRef": Ref,
+        "AttachableRef": AttachableRef,
+        "CashBack": CashBackInfo,
     }
 
     list_dict = {
@@ -45,8 +103,19 @@ class Deposit(QuickbooksManagedObject, LinkedTxnMixin):
     def __init__(self):
         super(Deposit, self).__init__()
         self.TotalAmt = 0
+        self.HomeTotalAmt = 0
         self.TxnDate = ""
+        self.DocNumber = ""
+        self.ExchangeRate = 1
+        self.GlobalTaxCalculation = "TaxExcluded"
+        self.PrivateNote = ""
+        self.TxnStatus = ""
+        self.TxnSource = ""
+
         self.DepositToAccountRef = None
+        self.DepartmentRef = None
+        self.CurrencyRef = None
+        self.AttachableRef = None
         self.Line = []
 
     def __unicode__(self):
--- tests/unit/objects/test_deposit.py
+++ tests/unit/objects/test_deposit.py
@@ -1,6 +1,6 @@
 import unittest
 
-from quickbooks.objects.deposit import Deposit, DepositLine
+from quickbooks.objects.deposit import Deposit, DepositLine, AttachableRef, CashBackInfo
 
 
 class DepositTests(unittest.TestCase):
@@ -17,3 +17,23 @@ class DepositLineTests(unittest.TestCase):
         deposit.Amount = 100
 
         self.assertEquals(unicode(deposit), "100")
+
+
+class AttachableRefTests(unittest.TestCase):
+    def test_init(self):
+        attachable_ref = AttachableRef()
+
+        self.assertEquals(attachable_ref.LineInfo, "")
+        self.assertFalse(attachable_ref.IncludeOnSend)
+        self.assertFalse(attachable_ref.Inactive)
+        self.assertFalse(attachable_ref.NoRefOnly)
+        self.assertEquals(attachable_ref.EntityRef, None)
+
+
+class CashBackInfoTests(unittest.TestCase):
+    def test_init(self):
+        cash_back_info = CashBackInfo()
+
+        self.assertEquals(cash_back_info.Amount, 0)
+        self.assertEquals(cash_back_info.Memo, "")
+        self.assertEquals(cash_back_info.AccountRef, None)
