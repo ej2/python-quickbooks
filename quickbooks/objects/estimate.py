575f08a01d86ca3bf020472d5ae21615a8efef52
ej2 <edward.emanuel@gmail.com>
2015-09-10 12:49:53 -0500

    Updated Estimate.

--- quickbooks/objects/estimate.py
+++ quickbooks/objects/estimate.py
@@ -1,37 +1,142 @@
 from base import QuickbooksBaseObject, CustomField, Ref, CustomerMemo, Address, EmailAddress, QuickbooksManagedObject, \
-    LinkedTxnMixin, QuickbooksTransactionEntity
+    LinkedTxnMixin, QuickbooksTransactionEntity, LinkedTxn
 from tax import TxnTaxDetail
+
+
+class EstimateLine(QuickbooksBaseObject):
+    list_dict = {
+        "LinkedTxn": LinkedTxn,
+        "CustomField": CustomField,
+    }
+
+    def __init__(self):
+        super(EstimateLine, self).__init__()
+        self.Id = 0
+        self.LineNum = 0
+        self.Description = ""
+        self.Amount = 0
+        self.DetailType = ""
+        self.LinkedTxn = []
+        self.CustomField = []
+
+    def __unicode__(self):
+        return str(self.Amount)
+
+
+class DiscountOverride(QuickbooksBaseObject):
+    class_dict = {
+        "DiscountRef": Ref,
+        "DiscountAccountRef": Ref,
+    }
+
+    qbo_object_name = "DiscountOverride"
+
+    def __init__(self):
+        super(DiscountOverride, self).__init__()
+        self.PercentBased = False
+        self.DiscountPercent = 0
+        self.DiscountRef = None
+        self.DiscountAccountRef = None
+
+
+class DiscountLineDetail(QuickbooksBaseObject):
+    class_dict = {
+        "Discount": DiscountOverride,
+        "ClassRef": Ref,
+        "TaxCodeRef": Ref,
+    }
+
+    def __init__(self):
+        super(DiscountLineDetail, self).__init__()
+
+        self.Discount = None
+        self.ClassRef = None
+        self.TaxCodeRef = None
+
+
+class DiscountLine(EstimateLine):
+    class_dict = {
+        "DiscountLineDetail": DiscountLineDetail
+    }
+
+    def __init__(self):
+        super(DiscountLine, self).__init__()
+        self.DetailType = "DiscountLineDetail"
+        self.DiscountLineDetail = None
+
+
+class SubtotalLineDetail(QuickbooksBaseObject):
+    class_dict = {
+        "ItemRef": Ref
+    }
+
+    def __init__(self):
+        super(SubtotalLineDetail, self).__init__()
+        self.ItemRef = None
+
+
+class SubtotalLine(EstimateLine):
+    class_dict = {
+        "SubtotalLineDetail": SubtotalLineDetail
+    }
+
+    def __init__(self):
+        super(SubtotalLine, self).__init__()
+        self.DetailType = "SubtotalLineDetail"
+        self.SubtotalLineDetail = None
+
+
+class DescriptionLineDetail(QuickbooksBaseObject):
+    class_dict = {
+        "TaxCodeRef": Ref
+    }
+
+    def __init__(self):
+        super(DescriptionLineDetail, self).__init__()
+        self.ServiceDate = ""
+        self.TaxCodeRef = None
+
+
+class DescriptionLine(EstimateLine):
+    class_dict = {
+        "DescriptionLineDetail": DescriptionLineDetail
+    }
+
+    def __init__(self):
+        super(DescriptionLine, self).__init__()
+        self.DetailType = "DescriptionOnly"
+        self.DescriptionLineDetail = None
+
 
 class SalesItemLineDetail(QuickbooksBaseObject):
     class_dict = {
         "ItemRef": Ref,
-        "TaxCodeRef": Ref
+        "ClassRef": Ref,
+        "TaxCodeRef": Ref,
+        "PriceLevelRef": Ref,
     }
 
     def __init__(self):
         super(SalesItemLineDetail, self).__init__()
         self.UnitPrice = 0
         self.Qty = 0
+        self.MarkupInfo = ""
+        self.ServiceDate = ""
+        self.TaxInclusiveAmt = 0
 
     def __unicode__(self):
         return str(self.UnitPrice)
 
 
-class EstimateLine(QuickbooksBaseObject):
+class SaleItemLine(EstimateLine):
     class_dict = {
         "SalesItemLineDetail": SalesItemLineDetail
     }
 
     def __init__(self):
-        super(EstimateLine, self).__init__()
-        self.LineNum = 0
-        self.Amount = 0
-        self.Description = ""
-        self.DetailType = ""
+        super(SaleItemLine, self).__init__()
+        self.DetailType = "SalesItemLineDetail"
         self.SalesItemLineDetail = None
-
-    def __unicode__(self):
-        return str(self.Amount)
 
 
 class Estimate(QuickbooksManagedObject, QuickbooksTransactionEntity, LinkedTxnMixin):
@@ -47,10 +152,17 @@ class Estimate(QuickbooksManagedObject, QuickbooksTransactionEntity, LinkedTxnMi
         "TxnTaxDetail": TxnTaxDetail,
         "CustomerMemo": CustomerMemo,
         "BillEmail": EmailAddress,
+        "DepartmentRef": Ref,
+        "CurrencyRef": Ref,
+        "ClassRef": Ref,
+        "SalesTermRef": Ref,
+        "ShipMethodRef": Ref,
     }
 
     list_dict = {
         "CustomField": CustomField,
+        "LinkedTxn": LinkedTxn,
+        "Line": EstimateLine,
     }
 
     qbo_object_name = "Estimate"
@@ -60,16 +172,31 @@ class Estimate(QuickbooksManagedObject, QuickbooksTransactionEntity, LinkedTxnMi
         self.DocNumber = ""
         self.TxnDate = ""
         self.TxnStatus = ""
+        self.PrivateNote = ""
         self.TotalAmt = 0
-        self.ApplyTaxAfterDiscount = 0
+        self.ExchangeRate = 1
+        self.ApplyTaxAfterDiscount = False
         self.PrintStatus = "NotSet"
         self.EmailStatus = "NotSet"
+        self.DueDate = ""
+        self.ShipDate = ""
+        self.ExpirationDate = ""
+        self.AcceptedBy = ""
+        self.AcceptedDate = ""
+        self.GlobalTaxCalculation = "TaxExcluded"
         self.BillAddr = None
         self.ShipAddr = None
+        self.BillEmail = None
         self.CustomerRef = None
         self.TxnTaxDetail = None
         self.CustomerMemo = None
+        self.ClassRef = None
+        self.SalesTermRef = None
+        self.ShipMethodRef = None
+
         self.CustomField = []
+        self.LinkedTxn = []
+        self.Line = []
 
     def __unicode__(self):
         return str(self.TotalAmt)
--- tests/unit/objects/test_estimate.py
+++ tests/unit/objects/test_estimate.py
@@ -1,6 +1,7 @@
 import unittest
 
-from quickbooks.objects.estimate import Estimate, EstimateLine, SalesItemLineDetail
+from quickbooks.objects.estimate import Estimate, EstimateLine, SalesItemLineDetail, DiscountOverride, \
+    DiscountLineDetail, SubtotalLine, DescriptionLineDetail, DescriptionLine, SaleItemLine, DiscountLine
 
 
 class EstimateTests(unittest.TestCase):
@@ -25,3 +26,62 @@ class SalesItemLineDetailTests(unittest.TestCase):
         sales_detail.UnitPrice = 10
 
         self.assertEquals(unicode(sales_detail), "10")
+
+
+class DiscountOverrideTests(unittest.TestCase):
+    def test_init(self):
+        discount_override = DiscountOverride()
+
+        self.assertEquals(discount_override.DiscountPercent, 0)
+        self.assertEquals(discount_override.DiscountRef, None)
+        self.assertEquals(discount_override.DiscountAccountRef, None)
+        self.assertFalse(discount_override.PercentBased)
+
+
+class DiscountLineDetailTesets(unittest.TestCase):
+    def test_init(self):
+        discount_detail = DiscountLineDetail()
+
+        self.assertEquals(discount_detail.Discount, None)
+        self.assertEquals(discount_detail.ClassRef, None)
+        self.assertEquals(discount_detail.TaxCodeRef, None)
+
+
+class SubtotalLineTest(unittest.TestCase):
+    def test_init(self):
+        subtotal_line = SubtotalLine()
+
+        self.assertEquals(subtotal_line.DetailType, "SubtotalLineDetail")
+        self.assertEquals(subtotal_line.SubtotalLineDetail, None)
+
+
+class DescriptionLineDetailTest(unittest.TestCase):
+    def test_init(self):
+        description_detail = DescriptionLineDetail()
+
+        self.assertEquals(description_detail.ServiceDate, "")
+        self.assertEquals(description_detail.TaxCodeRef, None)
+
+
+class DescriptionLineTest(unittest.TestCase):
+    def test_init(self):
+        line = DescriptionLine()
+
+        self.assertEquals(line.DetailType, "DescriptionOnly")
+        self.assertEquals(line.DescriptionLineDetail, None)
+
+
+class SaleItemLineTest(unittest.TestCase):
+    def test_init(self):
+        line = SaleItemLine()
+
+        self.assertEquals(line.DetailType, "SalesItemLineDetail")
+        self.assertEquals(line.SalesItemLineDetail, None)
+
+
+class DiscountLineTest(unittest.TestCase):
+    def test_init(self):
+        line = DiscountLine()
+
+        self.assertEquals(line.DetailType, "DiscountLineDetail")
+        self.assertEquals(line.DiscountLineDetail, None)
