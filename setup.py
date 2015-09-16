69a569fff59442cbe795891d2a6c18776e84321e
ej2 <edward.emanuel@gmail.com>
2015-09-16 15:50:48 -0500

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'setuptools',
         'rauth>=0.7.1',
-        'requests>=2.7.0'
+        'requests>=2.7.0',
         'simplejson>=2.2.0',
         'six>=1.4.0',
         'python-dateutil',
