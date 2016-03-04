ef1570ddbe18bc85f73c901438fd3eefdef3fd44
Richard Hajdu <tuskone16@gmail.com>
2016-03-04 10:20:33 +0100

    authorize url keeps the query parameters
    Authorize URL keeps repeating the old oauth_token which leads to ?oauth_token=abc123?oauth_token=cba321. That raises internal error within QBO.

--- quickbooks/client.py
+++ quickbooks/client.py
@@ -116,6 +116,7 @@ class QuickBooks(object):
         Returns the Authorize URL as returned by QB, and specified by OAuth 1.0a.
         :return URI:
         """
+        self.authorize_url = self.authorize_url[:self.authorize_url.find('?')]
         if self.qbService is None:
             self.set_up_service()
 
