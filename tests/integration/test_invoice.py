ba0600a8a6e492031980546b62fd1eb4c9f16172
FabioFleitas <fabio@tesorio.com>
2017-02-09 19:44:04 -0800

    Added InvoiceTest.test_delete test case

--- tests/integration/test_invoice.py
+++ tests/integration/test_invoice.py
@@ -46,3 +46,31 @@ class InvoiceTest(unittest.TestCase):
         self.assertEquals(query_invoice.CustomerMemo.value, "Customer Memo")
         self.assertEquals(query_invoice.Line[0].Description, "description")
         self.assertEquals(query_invoice.Line[0].Amount, 100.0)
+
+    def test_delete(self):
+        # First create an invoice
+        invoice = Invoice()
+
+        line = SalesItemLine()
+        line.LineNum = 1
+        line.Description = "description"
+        line.Amount = 100
+        line.SalesItemLineDetail = SalesItemLineDetail()
+        item = Item.all(max_results=1, qb=self.qb_client)[0]
+
+        line.SalesItemLineDetail.ItemRef = item.to_ref()
+        invoice.Line.append(line)
+
+        customer = Customer.all(max_results=1, qb=self.qb_client)[0]
+        invoice.CustomerRef = customer.to_ref()
+
+        invoice.CustomerMemo = CustomerMemo()
+        invoice.CustomerMemo.value = "Customer Memo"
+        invoice.save(qb=self.qb_client)
+
+        # Then delete
+        invoice_id = invoice.Id
+        invoice.delete(qb=self.qb_client)
+
+        query_invoice = Invoice.filter(Id=invoice_id, qb=self.qb_client)
+        self.assertEqual([], query_invoice)
