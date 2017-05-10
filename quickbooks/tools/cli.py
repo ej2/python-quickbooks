fcf2399bfe57f6787ff7aad3fa3949ee5f4c2cfa
Humberto Rocha <humrochagf@gmail.com>
2017-05-10 00:10:21 -0300

    improve command parameters and description

--- quickbooks/tools/cli.py
+++ quickbooks/tools/cli.py
@@ -6,19 +6,20 @@ from auth import handle_auth
 class CLI(argparse.ArgumentParser):
     info = ({
         'prog': 'quickbooks-cli',
-        'description': 'Starts a local server to help on getting token',
-        'usage': '%(prog)s key secret'
+        'description': 'Starts a local server to handle quickbooks auth',
     })
 
     def __init__(self):
         super(CLI, self).__init__(**self.info)
 
-        self.add_argument('consumer_key', type=str, help='consumer key')
-        self.add_argument('consumer_secret', type=str, help='consumer secret')
-        self.add_argument('--sandbox', action='store_true', dest='sandbox',
-                          help='sandbox flag')
-        self.add_argument('--port', type=int, default=8080, dest='port',
-                          help='auth calback port')
+        self.add_argument('consumer_key', type=str,
+                          help='quickbooks consumer key')
+        self.add_argument('consumer_secret', type=str,
+                          help='quickbooks consumer secret')
+        self.add_argument('-s', '-sandbox', action='store_true',
+                          dest='sandbox', help='sandbox flag')
+        self.add_argument('-p', '--port', type=int, default=8080,
+                          dest='port', help='auth calback port')
 
     def run(self, args=None):
         handle_auth(args.consumer_key, args.consumer_secret,
