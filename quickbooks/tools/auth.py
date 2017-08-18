b24514f8198969d8ead088e058bd4273c61e43c9
Edward Emanuel Jr <edward.emanuel@gmail.com>
2017-08-18 14:59:41 -0700

    Updated CLI.

--- quickbooks/tools/auth.py
+++ quickbooks/tools/auth.py
@@ -1,4 +1,4 @@
-from quickbooks.auth import Oauth1SessionManager
+from quickbooks.auth import Oauth1SessionManager, Oauth2SessionManager
 
 try:  # Python 3
     from urllib.parse import parse_qs, urlparse
@@ -19,6 +19,15 @@ class QuickBooksAuthHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        qb_data = self.server.qb_data
+        oauth_version = qb_data['oauth_version']
+
+        if oauth_version == 1:
+            self.get_oauth_1()
+        elif oauth_version == 2:
+            self.get_oauth_2()
+
+    def get_oauth_1(self):
         self._set_headers()
         qb_data = self.server.qb_data
 
@@ -52,6 +61,7 @@ class QuickBooksAuthHandler(BaseHTTPRequestHandler):
                       'UTF-8'))
             self.wfile.write(
                 bytes('<p><b>Realm Id:</b> {0}</p>'.format(realm_id), 'UTF-8'))
+
             self.wfile.write(
                 bytes('<p><b>Access Token:</b> {0}</p>'.format(
                     client.access_token), 'UTF-8'))
@@ -62,28 +72,98 @@ class QuickBooksAuthHandler(BaseHTTPRequestHandler):
             self.wfile.write(
                 bytes('<h1>QuickBooks auth failed, try again.</h1>', 'UTF-8'))
 
+    def get_oauth_2(self):
+        self._set_headers()
+        qb_data = self.server.qb_data
+
+        GET = parse_qs(urlparse(self.path).query)
+
+        auth_code = GET.get('code')
+
+        if auth_code:
+            client = Oauth2SessionManager(
+                sandbox=qb_data['sandbox'],
+                client_id=qb_data['consumer_key'],
+                client_secret=qb_data['consumer_secret'],
+                callback_url=qb_data['callback_url'],
+                base_url=qb_data['base_url'],
+            )
+
+            client.get_access_tokens(auth_code)
+
+            self.wfile.write(
+                bytes('<h1>QuickBooks auth handled with success!</h1>',
+                      'UTF-8'))
+            self.wfile.write(
+                bytes('<p><b>Sandbox:</b> {0}</p>'.format(qb_data['sandbox']),
+                      'UTF-8'))
+            self.wfile.write(
+                bytes('<p><b>Realm Id:</b> {0}</p>'.format(realm_id), 'UTF-8'))
+
+            self.wfile.write(
+                bytes('<p><b>Access Token:</b> {0}</p>'.format(
+                    client.access_token), 'UTF-8'))
+            self.wfile.write(
+                bytes('<p><b>Refresh Token:</b> {0}</p>'.format(
+                    client.refresh_token), 'UTF-8'))
+            self.wfile.write(
+                bytes('<p><b>Expires In:</b> {0}</p>'.format(
+                    client.expires_in), 'UTF-8'))
+        else:
+            self.wfile.write(
+                bytes('<h1>QuickBooks auth failed, try again.</h1>', 'UTF-8'))
+
 
 class QuickBooksAuthServer(HTTPServer):
 
     qb_client_class = Oauth1SessionManager
 
     @classmethod
-    def build_server(cls, consumer_key, consumer_secret, sandbox, port):
-        client = cls.qb_client_class(
-            sandbox=sandbox,
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            callback_url='http://localhost:{0}'.format(port)
-        )
+    def build_server(cls, consumer_key, consumer_secret, sandbox, port, oauth_version):
+        if oauth_version == 1:
+            client = Oauth1SessionManager(
+                sandbox=sandbox,
+                consumer_key=consumer_key,
+                consumer_secret=consumer_secret,
+                callback_url='http://localhost:{0}'.format(port)
+            )
 
-        qb_data = {
-            'authorize_url': client.get_authorize_url(),
-            'request_token': client.request_token,
-            'request_token_secret': client.request_token_secret,
-            'consumer_key': consumer_key,
-            'consumer_secret': consumer_secret,
-            'sandbox': sandbox,
-        }
+            qb_data = {
+                'authorize_url': client.get_authorize_url(),
+                'request_token': client.request_token,
+                'request_token_secret': client.request_token_secret,
+                'consumer_key': consumer_key,
+                'consumer_secret': consumer_secret,
+                'sandbox': sandbox,
+                'oauth_version': oauth_version,
+            }
+
+        elif oauth_version == 2:
+            client = Oauth2SessionManager(
+                sandbox=sandbox,
+                client_id=consumer_key,
+                client_secret=consumer_secret,
+                callback_url='http://localhost:{0}'.format(port),
+                base_url='http://localhost:{0}'.format(port),
+            )
+
+            qb_data = {
+                'authorize_url': client.get_authorize_url(),
+                'access_token': client.access_token,
+                'refresh_token': client.refresh_token,
+                'consumer_key': consumer_key,
+                'consumer_secret': consumer_secret,
+                'token_type': client.token_type,
+                'expires_in': client.expires_in,
+                'x_refresh_token_expires_in': client.x_refresh_token_expires_in,
+                'sandbox': sandbox,
+                'oauth_version': oauth_version,
+                'callback_url': 'http://localhost:{0}'.format(port),
+                'base_url': 'http://localhost:{0}'.format(port),
+            }
+
+        else:
+            raise Exception('Invalid OAuth version number. Version number must be 1 or 2.')
 
         instance = cls(('', port), QuickBooksAuthHandler)
         instance.qb_data = qb_data
--- quickbooks/tools/cli.py
+++ quickbooks/tools/cli.py
@@ -20,12 +20,14 @@ class CLI(argparse.ArgumentParser):
                           dest='sandbox', help='sandbox flag')
         self.add_argument('-p', '--port', type=int, default=8080,
                           dest='port', help='auth calback port')
+        self.add_argument('oauth_version', type=int, default=2,
+                          help='OAuth version number')
 
     def run(self, args=None):
         print('Starting the authentication process...')
 
         server = QuickBooksAuthServer.build_server(
-            args.consumer_key, args.consumer_secret, args.sandbox, args.port)
+            args.consumer_key, args.consumer_secret, args.sandbox, args.port, args.oauth_version)
 
         print('Copy and paste the authorization url on your browser:')
         print(server.qb_data['authorize_url'])
