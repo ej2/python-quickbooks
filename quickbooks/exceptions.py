ad18e9b5b4cb0d7c2358a6726e135dcf5a57b751
varun-bheemaiah <varunbheemaiah9@deshik.in>
2021-11-24 16:32:10 +0530

    Added more detailed messages in case of an exception

--- quickbooks/exceptions.py
+++ quickbooks/exceptions.py
@@ -21,33 +21,38 @@ class UnsupportedException(QuickbooksException):
     """
     Quickbooks Error Codes from 500 to 599
     """
-    pass
+    def __str__(self):
+        return "QB Unsupported Exception: " + self.message + " \n\n" + self.detail
 
 
 class GeneralException(QuickbooksException):
     """
     Quickbooks Error Codes from 600 to 1999
     """
-    pass
+    def __str__(self):
+        return "QB General Exception: " + self.message + " \n\n" + self.detail
 
 
 class ValidationException(QuickbooksException):
     """
     Quickbooks Error Codes from 2000 to 4999
     """
-    pass
+    def __str__(self):
+        return "QB Validation Exception: " + self.message + " \n\n" + self.detail
 
 
 class SevereException(QuickbooksException):
     """
     Quickbooks Error Codes greater than 10000
     """
-    pass
+    def __str__(self):
+        return "QB Severe Exception: " + self.message + " \n\n" + self.detail
 
 
 class ObjectNotFoundException(QuickbooksException):
     """
     Quickbooks Error Code 610
     """
-    pass
+    def __str__(self):
+        return "QB Object Not Found Exception: " + self.message + " \n\n" + self.detail
 
