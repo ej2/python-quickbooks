1dc7f6878092534687827511fd91733a350b85f1
William N. Green <william.green@gmail.com>
2025-03-16 13:33:08 -0700

    cleanup version number default

--- quickbooks/client.py
+++ quickbooks/client.py
@@ -76,14 +76,19 @@ class QuickBooks(object):
         if 'company_id' in kwargs:
             instance.company_id = kwargs['company_id']
 
-        if 'minorversion' in kwargs:
-            instance.minorversion = kwargs['minorversion']
-
-            if instance.minorversion < instance.MINIMUM_MINOR_VERSION:
-                warnings.warn(
-                    'Minor Version no longer supported.'
-                    'See: https://blogs.intuit.com/2025/01/21/changes-to-our-accounting-api-that-may-impact-your-application/',
-                    DeprecationWarning)
+        # Handle minorversion with default
+        instance.minorversion = kwargs.get('minorversion', instance.MINIMUM_MINOR_VERSION)
+        if 'minorversion' not in kwargs:
+            warnings.warn(
+                'No minor version specified. Defaulting to minimum supported version (75). '
+                'Please specify minorversion explicitly when initializing QuickBooks. '
+                'See: https://blogs.intuit.com/2025/01/21/changes-to-our-accounting-api-that-may-impact-your-application/',
+                DeprecationWarning)
+        elif instance.minorversion < instance.MINIMUM_MINOR_VERSION:
+            warnings.warn(
+                f'Minor Version {instance.minorversion} is no longer supported. Minimum supported version is {instance.MINIMUM_MINOR_VERSION}. '
+                'See: https://blogs.intuit.com/2025/01/21/changes-to-our-accounting-api-that-may-impact-your-application/',
+                DeprecationWarning)
 
         instance.invoice_link = kwargs.get('invoice_link', False)
 
@@ -157,15 +162,7 @@ class QuickBooks(object):
         if not params:
             params = {}
 
-        if self.minorversion is None:
-            warnings.warn(
-                'No minor version specified. Defaulting to minimum supported version (75). '
-                'Please specify minorversion explicitly when initializing QuickBooks. '
-                'See: https://blogs.intuit.com/2025/01/21/changes-to-our-accounting-api-that-may-impact-your-application/',
-                DeprecationWarning)
-            params['minorversion'] = self.MINIMUM_MINOR_VERSION
-        else:
-            params['minorversion'] = self.minorversion
+        params['minorversion'] = self.minorversion
         
         if request_id:
             params['requestid'] = request_id
@@ -240,10 +237,18 @@ class QuickBooks(object):
             return result
 
     def get(self, *args, **kwargs):
-        return self.make_request("GET", *args, **kwargs)
+        if 'params' not in kwargs:
+            kwargs['params'] = {}
+        if 'minorversion' not in kwargs['params']:
+            kwargs['params']['minorversion'] = self.MINIMUM_MINOR_VERSION
+        return self.make_request('GET', *args, **kwargs)
 
     def post(self, *args, **kwargs):
-        return self.make_request("POST", *args, **kwargs)
+        if 'params' not in kwargs:
+            kwargs['params'] = {}
+        if 'minorversion' not in kwargs['params']:
+            kwargs['params']['minorversion'] = self.MINIMUM_MINOR_VERSION
+        return self.make_request('POST', *args, **kwargs)
 
     def process_request(self, request_type, url, headers="", params="", data=""):
         if self.session is None:
@@ -256,9 +261,9 @@ class QuickBooks(object):
 
     def get_single_object(self, qbbo, pk, params=None):
         url = "{0}/company/{1}/{2}/{3}/".format(self.api_url, self.company_id, qbbo.lower(), pk)
-        result = self.get(url, {}, params=params)
-
-        return result
+        if params is None:
+            params = {}
+        return self.get(url, {}, params=params)
 
     @staticmethod
     def handle_exceptions(results):
@@ -316,9 +321,9 @@ class QuickBooks(object):
 
     def update_object(self, qbbo, request_body, _file_path=None, request_id=None, params=None):
         url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id,  qbbo.lower())
-        result = self.post(url, request_body, file_path=_file_path, request_id=request_id, params=params)
-
-        return result
+        if params is None:
+            params = {}
+        return self.post(url, request_body, file_path=_file_path, request_id=request_id, params=params)
 
     def delete_object(self, qbbo, request_body, _file_path=None, request_id=None):
         url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id, qbbo.lower())
